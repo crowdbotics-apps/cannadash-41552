f679ba3b86a4bbe7c593dd9995d8dedd52dc312c
Crowdbotics <team@crowdbotics.com>
2023-06-17 03:23:44 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "cannadash-41552",
+  url: "https://cannadash-41552.botics.co",
+  api: "https://cannadash-41552.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
